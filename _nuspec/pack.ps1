afa0ee95e6a354a77c8bfd3fde909e856238baf9
Federico Barresi <federico.barresi@evopro-ag.de>
2018-11-06 14:11:41 +0100

    added nuspec

---
+++ _nuspec/pack.ps1
@@ -0,0 +1,11 @@
+$version = [System.Reflection.Assembly]::LoadFile("C:\projects\Sharp7Reactive\Sharp7.Rx\bin\Release\netstandard2.0\Sharp7.Rx.dll).GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content _nuspec\Sharp7.Rx.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File _nuspec\Sharp7.Rx.compiled.nuspec
+
+& _nuspec\NuGet.exe pack _nuspec\Sharp7.Rx.compiled.nuspec
