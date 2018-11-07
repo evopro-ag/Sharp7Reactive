4156d5cd14fae0772f8d1c01ac0374d2d6392dc3
Federico Barresi <federico.barresi@evopro-ag.de>
2018-11-07 08:45:05 +0100

    fixed version patching issue

--- _nuspec/pack.ps1
+++ _nuspec/pack.ps1
@@ -1,10 +1,7 @@
-$version = [System.Reflection.Assembly]::LoadFile("C:\projects\Sharp7Reactive\Sharp7.Rx\bin\Release\netstandard2.0\Sharp7.Rx.dll").GetName().Version
-$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-
-Write-Host "Setting .nuspec version tag to $versionStr oder $env:appveyor_build_version"
+Write-Host "Setting .nuspec version tag to $env:appveyor_build_version"
 
 $content = (Get-Content _nuspec\Sharp7.Rx.nuspec) 
-$content = $content -replace '\$version\$',$versionStr
+$content = $content -replace '\$version\$', $env:appveyor_build_version
 
 $content | Out-File _nuspec\Sharp7.Rx.compiled.nuspec
 
