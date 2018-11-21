a3fd30a457b48dd63286dcd850fcfbf70d898479
Alexander Seminjakiw <alexander.seminjakiw@evopro-ag.de>
2018-11-21 09:34:06 +0100

    Update NuGet package generation to SDK style

--- _nuspec/pack.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Setting .nuspec version tag to $env:appveyor_build_version"
-
-$content = (Get-Content _nuspec\Sharp7.Rx.nuspec) 
-$content = $content -replace '\$version\$', $env:appveyor_build_version
-
-$content | Out-File _nuspec\Sharp7.Rx.compiled.nuspec
-
-& _nuspec\NuGet.exe pack _nuspec\Sharp7.Rx.compiled.nuspec
